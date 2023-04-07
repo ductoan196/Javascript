a7b28a797ef881f71cbd764e365555e6fcbb7949
ductoan196 <ductoannd96@gmail.com>
2023-04-07 22:52:45 +0700

    update

---
+++ homework_day02_3.js
@@ -0,0 +1,9 @@
+//Lab1: 
+let arr1= [1,2,3,4,5]
+function checkElementExist(arr,num){
+    return arr.includes(num)
+}
+console.log(checkElementExist(arr1,5))
+console.log(checkElementExist(arr1,6))
+
+//Lab2: 
