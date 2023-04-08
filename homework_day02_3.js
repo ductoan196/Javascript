0b95be2b37d8e89a176ee600246cad30cd815cb5
ductoan196 <ductoannd96@gmail.com>
2023-04-08 13:10:11 +0700

    update

--- homework_day02_3.js
+++ homework_day02_3.js
@@ -4,3 +4,17 @@ function sum(arr) {
   for (let i = 0; i < arr.length; i++) total += arr[i];
   return total;
 }
+
+function multiple(arr) {
+  let result = 1;
+  for (let i = 0; i < arr.length; i++) result *= arr[i];
+  return result;
+}
+
+function average(arr){
+  return result= sum(arr)/arr.length
+}
+
+function bigger_key(arr, key){
+  return arr.filter(x => x>key)
+}
